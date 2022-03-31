3bdde45143ae20edeaf0daebeb78ba477cb5a080
Harvey <31661937+HarveyKeitel@users.noreply.github.com>
2022-03-31 14:36:29 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,3 +3,22 @@ module.exports = {
       logo: 'portfolios/images/logo.jpg',
     }
   }
+
+module.exports = {
+    themeConfig: {
+        nav: [
+        
+            {
+                title: ':bank: Portfolios',
+                path: '/',
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "IQ Protocol", path: "/handbook/IQ" }
+                ]
+            },
+            {
+              
+            }
+          ]
+    }
+}  
